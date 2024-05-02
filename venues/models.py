f33b486a0d0513f6b884558566555120f14a86fa
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2024-05-02 11:16:25 +0200

    initial commit

---
+++ venues/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    categories = models.ManyToManyField(Category, related_name="venues")
+
+    def __str__(self):
+        return self.name
+
+
+
