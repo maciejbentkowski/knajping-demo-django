648beb68950c9a706bdc0fe4908f6415b7687daf
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2024-09-26 18:33:01 +0200

    Change static files dirs

--- knajping/settings.py
+++ knajping/settings.py
@@ -161,8 +161,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "staticfiles/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "theme/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
